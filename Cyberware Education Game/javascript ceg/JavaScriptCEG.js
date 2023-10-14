f2a60ca2394e9dfd867b8850e5d5e8140b34a88a
kimi246 <146855048+kimi246@users.noreply.github.com>
2023-10-15 00:02:00 +0800

    Cyberware Education Game Website
    Deenesh, please take note on our website. Make sure to update the website

---
+++ Cyberware Education Game/javascript ceg/JavaScriptCEG.js
@@ -0,0 +1,7 @@
+﻿// JavaScript function to show an alert when the button is clicked
+function showAlert() {
+    alert("Thank you for your interest! This is just a demo.");
+}
+
+// Attach the showAlert function to the button's click event
+document.getElementById("learnMoreBtn").addEventListener("click", showAlert);
