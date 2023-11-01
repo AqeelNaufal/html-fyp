7f7ab9468cc6eb2d05ef693925308b8f1592a5d6
kimi246 <146855048+kimi246@users.noreply.github.com>
2023-11-01 14:41:11 +0800

    Delete Cyberware Education Game directory

--- Cyberware Education Game/javascript ceg/JavaScriptCEG.js
+++
@@ -1,7 +0,0 @@
-﻿// JavaScript function to show an alert when the button is clicked
-function showAlert() {
-    alert("Thank you for your interest! This is just a demo.");
-}
-
-// Attach the showAlert function to the button's click event
-document.getElementById("learnMoreBtn").addEventListener("click", showAlert);
